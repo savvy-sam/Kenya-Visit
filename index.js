f2c00119f76b24a4692608eaab4f28c7fb70ed2b
Samson Chege <samchege09@gmail.com>
2022-12-31 03:26:41 -0500

    Added Logic for Database Connection

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const mongoose= require('mongoose');
+require('dotenv').config();
+
 
 const app=express();
-
 app.use(express.json());
+
+const mongoString = process.env.DATABASE_URL;
+
+mongoose.connect(mongoString);
+const database= mongoose.connection;
+
+database.on('error', (error) => {
+    console.log(error);
+});
+
+
+database.once('connected', () => {
+    console.log('Connection to the database is succesful');
+});
 
 
 const port = process.env.PORT || 3000 ;
