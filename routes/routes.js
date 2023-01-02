ee9f2884b64204f7f0ab97b79296ca3de9db6462
Samson Chege <samchege09@gmail.com>
2023-01-02 07:14:14 -0500

    Added Create and List Functionality for Admin category

--- routes/routes.js
+++ routes/routes.js
@@ -5,12 +5,33 @@ const Model = require('../model/model');
 
 //get all the admins
 
-router.get('/admins', (req, res) => {
-    res.send('get all api')
+router.get('/admins', async (req, res) => {
+    try{
+        const allData = await Model.find();
+        res.json(allData)
+    }
+
+    catch(error){
+        res.status(500).json({message: error.message})
+    }
 });
 
-router.post('/admin/create', (req, res) => {
-    res.send('Create Admin')
+router.post('/admin/create', async (req, res) => {
+    const data = new Model({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        passwordConfirmation: req.body.passwordConfirmation
+    })
+
+    try {
+        const adminData= await data.save();
+        res.status(200).json(adminData)
+    }
+    catch(error){
+        res.status(400).json({message: error.message})
+
+    }
 });
 
 router.get('/admin/:id', (req, res) => {
