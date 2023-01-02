b5aa309e74c7050916d8bf5e74657a6c579057bb
Samson Chege <samchege09@gmail.com>
2023-01-02 13:49:26 -0500

    Added Logic for Delete and Update Admin category

--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { findByIdAndUpdate } = require('../model/model');
 const router = express.Router();
 module.exports = router;
 const Model = require('../model/model');
@@ -34,16 +35,44 @@ router.post('/admin/create', async (req, res) => {
     }
 });
 
-router.get('/admin/:id', (req, res) => {
-    res.send(`${req.params.id}`)
+router.get('/admin/:id', async (req, res) => {
+    try{
+        data= await Model.findById(req.params.id);
+        res.status(200).json(data)
+    }
+
+    catch(error){
+        res.status(500).json({message: error.message})
+    }
 });
 
-router.patch('/admin/:id', (req, res) => {
-    res.send('Update individual admin')
+router.patch('/admin/update/:id', async (req, res) => {
+
+    try{
+    const id= req.params.id;
+    const updatedData= req.body;
+    const options = {new : true};
+
+    const result = await Model.findByIdAndUpdate(id, updatedData, options)
+
+    res.send(result)
+    }
+
+    catch(error){
+        res.status(400).json({message: error.message})
+    }
 });
 
-router.delete('/admin/:id', (req, res) => {
-    res.send('Delete the admin')
+router.delete('/admin/delete/:id', async (req, res) => {
+    try{
+        id= req.params.id;
+        data= await Model.findByIdAndDelete(id);
+        res.send(`User ${data.name} has been deleted...`)
+    }
+
+    catch(error){
+        res.status(500).json({'message': error.message})
+    }
 })
 
 
